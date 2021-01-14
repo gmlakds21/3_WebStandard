425fdbf94641adb71cee30d1ad4939142dff6627
Small_Chicken <74525961+gmlakds21@users.noreply.github.com>
2021-01-14 18:13:43 +0900

    Delete 4_Spring4/src/main/java/Seunghee directory

--- 4_Spring4/src/main/java/Seunghee/_interface/_01_14_04_HelloSpring4Interface04.java
+++
@@ -1,7 +0,0 @@
-package Seunghee._interface;
-
-public interface _01_14_04_HelloSpring4Interface04 {
-
-    void sayHello(String msg);
-
-}
--- 4_Spring4/src/main/java/Seunghee/_interface/_01_14_05_HelloSpring4Interface05.java
+++
@@ -1,7 +0,0 @@
-package Seunghee._interface;
-
-public interface _01_14_05_HelloSpring4Interface05 {
-
-    void sayHello(String msg);
-
-}
